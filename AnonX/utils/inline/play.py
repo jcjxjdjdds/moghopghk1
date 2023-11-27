b3bf99a6173138b9da9af3392186c990abeb88c6
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-11-27 14:24:10 +0200

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -61,7 +61,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 𖢻 ⌯ َِ𝙈َِ𝙐َِ𝙎َِ𝙄َِ𝘾 ⌯ ˹🎧˼⁩", url=f"https://t.me/pp_g3"
+                text="𝚂𝙾𝚄𝚁𝙲𝙴 𝙳𝙰𝚁𝙺⁩", url=f"https://t.me/T4_Mohamed"
             )
         ],
         [
@@ -123,7 +123,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 𖢻 ⌯ َِ𝙈َِ𝙐َِ𝙎َِ𝙄َِ𝘾 ⌯ ˹🎧˼⁩", url=f"https://t.me/pp_g3"
+                text="𝚂𝙾𝚄𝚁𝙲𝙴 𝙳𝙰𝚁𝙺⁩", url=f"https://t.me/T4_Mohamed"
             )
         ],
         [
@@ -157,7 +157,7 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 𖢻 ⌯ َِ𝙈َِ𝙐َِ𝙎َِ𝙄َِ𝘾 ⌯ ˹🎧˼⁩", url=f"https://t.me/pp_g3"
+                text="𝚂𝙾𝚄𝚁𝙲𝙴 𝙳𝙰𝚁𝙺⁩", url=f"https://t.me/T4_Mohamed"
             )
         ],
         [
@@ -188,7 +188,7 @@ def telegram_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 𖢻 ⌯ َِ𝙈َِ𝙐َِ𝙎َِ𝙄َِ𝘾 ⌯ ˹🎧˼⁩", url=f"https://t.me/pp_g3"
+                text="𝚂𝙾𝚄𝚁𝙲𝙴 𝙳𝙰𝚁𝙺⁩", url=f"https://t.me/T4_Mohamed"
             )
         ],
         [
@@ -348,7 +348,7 @@ def queue_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝘾𝙍 𖢻 ⌯ َِ𝙈َِ𝙐َِ𝙎َِ𝙄َِ𝘾 ⌯ ˹🎧˼⁩", url=f"https://t.me/pp_g3"
+                text="𝚂𝙾𝚄𝚁𝙲𝙴 𝙳𝙰𝚁𝙺⁩", url=f"https://t.me/T4_Mohamed"
             )
         ],
         [
