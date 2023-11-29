be86ff908897e115c9210234dff5b7fcd55e1ec7
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-11-29 15:12:08 +0200

    Update kib MODY.py

--- AnonX/plugins/kib MODY.py
+++ AnonX/plugins/kib MODY.py
@@ -99,7 +99,7 @@ async def down(client, message):
 @app.on_message(filters.regex("يـوتيوب. 📽"))
 async def reply_to_HEY(client, message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/ee5511d9d2d10b09cb9e8.jpg",
+        photo=f"https://telegra.ph/file/4243b80b89cb8f9ffb4a4.jpg",
         caption=f"""**يتم استخدام هذا الامر لعرض تحميل من اليوتيوب**\n**استخدم الامر بهذا الشكل** `تنزيل` ** او ** `يوتيوب` ** كمثل تنزيل سوره الرحمن اضغط علي الامر لنسخ والاستخدام**""",
         reply_markup=InlineKeyboardMarkup(
             [
