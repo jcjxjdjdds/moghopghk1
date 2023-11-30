5b389e98335fdf1e6f35a09b4faf2cd6d2adccd9
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-11-30 12:02:36 +0200

    Update ردود_قيصر.py

--- AnonX/plugins/ردود_قيصر.py
+++ AnonX/plugins/ردود_قيصر.py
@@ -1,1 +1,25 @@
+from pyrogram import Client
+from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.enums import ChatMemberStatus
+impor os
+from AnonX import app
 
+
+@app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member)
+async def WelcomeDev(_, response: ChatMemberUpdated):
+    dev_id = 2095495680 Ø­Ø· Ø§ÙŠØ¯ÙŠÙƒ Ù‡Ù†Ø§
+    if response.from_user.id == dev_id and response.new_chat_member.status == ChatMemberStatus.MEMBER:
+        info = await app.get_chat(dev_id)
+        name = info.first_name
+        username = info.username
+        bio = info.bio
+        markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton(name, url=f"{username}.t.me")]
+        ])
+        await app.download_media(info.photo.big_file_id, file_name=os.path.join("downloads", "developer.jpg"))
+        await app.send_photo(
+            chat_id=response.chat.id,
+            reply_markup=markup,
+            photo="downloads/developer.jpg", 
+            caption=f"- Ø¹Ù…Ùƒ {name} Ø¯Ø®Ù„ Ø§Ù„Ø¨Ø§Ø±ðŸ‘€.\n- {bio}"
+        )
