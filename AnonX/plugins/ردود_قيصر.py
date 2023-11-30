355882b91ecf528ad2d0d20a89361e457d13dcc8
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-11-30 11:53:11 +0200

    Update ردود_قيصر.py

--- AnonX/plugins/ردود_قيصر.py
+++ AnonX/plugins/ردود_قيصر.py
@@ -7,7 +7,7 @@ from AnonXMusic import app
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member)
 async def WelcomeDev(_, response: ChatMemberUpdated):
-    dev_id = ايديك# Ø­Ø· Ø§ÙŠØ¯ÙŠÙƒ Ù‡Ù†Ø§
+    dev_id = 2095495680 Ø­Ø· Ø§ÙŠØ¯ÙŠÙƒ Ù‡Ù†Ø§
     if response.from_user.id == dev_id and response.new_chat_member.status == ChatMemberStatus.MEMBER:
         info = await app.get_chat(dev_id)
         name = info.first_name
